fbe464916d04a97aeb32aba153dc9e7ec4eb8571
Simone Bellotti <simone.b93@hotmail.it>
2014-03-23 23:53:32 +0100

    Added depth transition

--- SwipeTabs/src/it/bellotti/android/swipetabs/activities/HomeActivity.java
+++ SwipeTabs/src/it/bellotti/android/swipetabs/activities/HomeActivity.java
@@ -11,12 +11,16 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 
 public class HomeActivity extends FragmentActivity implements ActionBar.TabListener {
 
     private ViewPager mViewPager;
     private TabsPagerAdapter mAdapter;
     private ActionBar mActionBar;
+    
 //    private PagerTabStrip mPagerTitleStrip;
 
 	@Override
@@ -31,20 +35,22 @@ public class HomeActivity extends FragmentActivity implements ActionBar.TabListe
 		mActionBar = getActionBar();
         mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
         
-        //PageTitleStrip
-//        mPagerTitleStrip = (PagerTabStrip) findViewById(R.id.pager_tab_strip);
         
         mViewPager.setAdapter(mAdapter);
-        mActionBar.setHomeButtonEnabled(false);
-        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);       
- 
-        // Adding Tabs
-        for (String tab_name : Constants.TABS) {
-        	mActionBar.addTab(mActionBar.newTab().setText(tab_name)
-                    .setTabListener(this));
-        }
         
-
+        //If you want to add the stable ActionBar
+        
+//        mActionBar.setHomeButtonEnabled(false);
+//        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);       
+ 
+        // Adding Tabs for stabled ActionBar
+//        for (String tab_name : Constants.TABS) {
+//        	mActionBar.addTab(mActionBar.newTab().setText(tab_name)
+//                    .setTabListener(this));
+//        }
+        
+        DepthPageTransformer callback = new DepthPageTransformer();
+        mViewPager.setPageTransformer(true, callback);
         
         
     	mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
@@ -55,7 +61,8 @@ public class HomeActivity extends FragmentActivity implements ActionBar.TabListe
                 // make respected tab selected
                 // When swiping between pages, select the
                 // corresponding tab.
-            	mActionBar.setSelectedNavigationItem(position);
+            	
+//            	mActionBar.setSelectedNavigationItem(position);
             }
 
             @Override
@@ -102,5 +109,42 @@ public class HomeActivity extends FragmentActivity implements ActionBar.TabListe
 		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
 	}
+	
+	public class DepthPageTransformer implements ViewPager.PageTransformer {
+	    private static final float MIN_SCALE = 0.75f;
 
+	    public void transformPage(View view, float position) {
+	        int pageWidth = view.getWidth();
+
+	        if (position < -1) { // [-Infinity,-1)
+	            // This page is way off-screen to the left.
+	            view.setAlpha(0);
+
+	        } else if (position <= 0) { // [-1,0]
+	            // Use the default slide transition when moving to the left page
+	            view.setAlpha(1);
+	            view.setTranslationX(0);
+	            view.setScaleX(1);
+	            view.setScaleY(1);
+
+	        } else if (position <= 1) { // (0,1]
+	            // Fade the page out.
+	            view.setAlpha(1 - position);
+
+	            // Counteract the default slide transition
+	            view.setTranslationX(pageWidth * -position);
+
+	            // Scale the page down (between MIN_SCALE and 1)
+	            float scaleFactor = MIN_SCALE
+	                    + (1 - MIN_SCALE) * (1 - Math.abs(position));
+	            view.setScaleX(scaleFactor);
+	            view.setScaleY(scaleFactor);
+
+	        } else { // (1,+Infinity]
+	            // This page is way off-screen to the right.
+	            view.setAlpha(0);
+	        }
+	    }
+	}
+	
 }
--- SwipeTabs/src/it/bellotti/android/swipetabs/fragments/HomeFragment.java
+++ SwipeTabs/src/it/bellotti/android/swipetabs/fragments/HomeFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,22 +20,39 @@ public class HomeFragment extends Fragment {
  
         View fRootView = inflater.inflate(R.layout.fragment_tab_one, container, false);
         
-        final EditText editText = (EditText) fRootView.findViewById(R.id.edit_text);
-
-        editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
-
-			@Override
-			public boolean onEditorAction(TextView v, int actionId,
-					KeyEvent event) {
-			       if (actionId == EditorInfo.IME_ACTION_DONE) {
-	                    editText.setText("ciaoooo");
-	                    return true;
-	                }
-	                return false;
-			}
-        });
+//        final EditText editText = (EditText) fRootView.findViewById(R.id.edit_text);
+//        editText.setHorizontallyScrolling(false);
+//        editText.setMaxLines(Integer.MAX_VALUE);
+//        editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+//
+//			@Override
+//			public boolean onEditorAction(TextView v, int actionId,
+//					KeyEvent event) {
+//			       if (actionId == EditorInfo.IME_ACTION_DONE) {
+//	                    editText.setText("ciaoooo");
+//	                    return true;
+//	                }
+//	                return false;
+//			}
+//        });
          
         return fRootView;
     }
+    
+//    @Override
+//    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+//        InputConnection connection = super.onCreateInputConnection(outAttrs);
+//        int imeActions = outAttrs.imeOptions&EditorInfo.IME_MASK_ACTION;
+//        if ((imeActions&EditorInfo.IME_ACTION_DONE) != 0) {
+//            // clear the existing action
+//            outAttrs.imeOptions ^= imeActions;
+//            // set the DONE action
+//            outAttrs.imeOptions |= EditorInfo.IME_ACTION_DONE;
+//        }
+//        if ((outAttrs.imeOptions&EditorInfo.IME_FLAG_NO_ENTER_ACTION) != 0) {
+//            outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
+//        }
+//        return connection;
+//    }
 
 }
